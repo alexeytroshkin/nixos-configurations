97b456a174c0f1b538416c52a1a1b1a6c59e3d54
Alexey Troshkin <alextroshkin@outlook.com>
2026-05-24 19:25:30 +0300

    97ea020d-bb2f-481c-9d97-90c11da51234

--- hosts/andromeda/configuration.nix
+++ hosts/andromeda/configuration.nix
@@ -171,6 +171,18 @@
     nvidiaBusId = "PCI:1:0:0";
   };
 
+  #----------------------------------------------------------------
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
